6acb41d279f35e6081a748bb3170c774baebb6b6
Tyrone Roberts <hi@tyronejroberts.co.uk>
2024-01-16 21:57:34 +0000

    Reduce uneccessary line breaks

--- src/main.cpp
+++ src/main.cpp
@@ -14,11 +14,7 @@ int main(int argc, char *argv[]) {
     while(game.isRunning()) {
         game.handleEvents();
         game.update();
-
-
-
         game.render();
-
         SDL_Delay(1000 / 15);
     }
 
